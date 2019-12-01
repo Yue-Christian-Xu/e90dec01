5e5c964091135e254c56e0eac38d8689450125ff
Yue Xu <yxu2@hotmail.com>
2019-12-01 14:01:18 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            E90 is an interesting course!
+          </p>
+        </header>
+      </div>
+    );    
+  }
+}
+
+export default withAuthenticator(App, true);
